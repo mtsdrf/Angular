d0758d8921e443cdbf9fa2ca132f06a6e23c8253
Mateus Ferreira <mtsferreira471@gmail.com>
2019-01-04 18:27:43 -0200

    Add bootstrap and new component

---
+++ cursoalura/src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ca-photo',
+    templateUrl: './photo.component.html'
+})
+
+export class PhotoComponent{
+    @Input() url: '';
+    @Input() desc: '';
+}
